5490e60bb143354da1ad7e00947c369fe3f984e4
andriyfm <andrifmsyh@gmail.com>
2020-03-28 13:03:56 +0700

    Fetch all the corona virus cases

---
+++ src/components/AllCases.vue
@@ -0,0 +1,25 @@
+<template>
+  <section>
+    <h3>{{ heading }}</h3>
+    <ul>
+      <li v-for="(value, key) in cases" :key="key">{{ key }}: {{ value }}</li>
+    </ul>
+  </section>
+</template>
+
+<script>
+export default {
+  data: () => ({ heading: "Get all NovelCovid Cases", cases: [] }),
+  mounted() {
+    this.fetchAll();
+  },
+  methods: {
+    fetchAll: async function() {
+      const { data } = await this.axios.get("https://corona.lmao.ninja/all");
+      this.cases = data;
+    }
+  }
+};
+</script>
+
+<style></style>
