d94e8711de1440c8094e77d5be61993de104e6aa
andriyfm <andrifmsyh@gmail.com>
2020-03-28 12:35:32 +0700

    Create about covid component

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,16 @@
 <template>
   <div id="app">
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <AboutCovid />
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import AboutCovid from "./components/AboutCovid";
 
 export default {
   name: "App",
   components: {
-    HelloWorld
+    AboutCovid
   }
 };
 </script>
---
+++ src/components/AboutCovid.vue
@@ -0,0 +1,49 @@
+<template>
+  <section>
+    <h3>Penyakit koronavirus 2019</h3>
+    <p>
+      Penyakit koronavirus 2019 (bahasa Inggris: coronavirus disease 2019,
+      disingkat COVID-19)[7][8] adalah penyakit menular yang disebabkan oleh
+      SARS-CoV-2, salah satu jenis koronavirus.[9][10] Penyakit ini
+      mengakibatkan pandemi koronavirus 2019–2020.[11][12] Penderita COVID-19
+      dapat mengalami demam, batuk kering, dan kesulitan bernapas.[6][13][14]
+      Sakit tenggorokan, pilek, atau bersin-bersin lebih jarang ditemukan.[15]
+      Pada penderita yang paling rentan, penyakit ini dapat berujung pada
+      pneumonia dan kegagalan multiorgan.[11][12]
+    </p>
+    <p>
+      Infeksi menyebar dari satu orang ke orang lain melalui percikan (droplet)
+      dari saluran pernapasan yang sering dihasilkan saat batuk atau
+      bersin.[12][16] Waktu dari paparan virus hingga timbulnya gejala klinis
+      berkisar antara 1–14 hari dengan rata-rata 5 hari.[12][17][18][19] Metode
+      standar diagnosis adalah uji reaksi berantai polimerase transkripsi-balik
+      (rRT-PCR) dari usap nasofaring atau sampel dahak dengan hasil dalam
+      beberapa jam hingga 2 hari. Pemeriksaan antibodi dari sampel serum darah
+      juga dapat digunakan dengan hasil dalam beberapa hari.[20] Infeksi juga
+      dapat didiagnosis dari kombinasi gejala, faktor risiko, dan pemindaian
+      tomografi terkomputasi pada dada yang menunjukkan gejala
+      pneumonia.[21][22]
+    </p>
+    <p>
+      Mencuci tangan, menjaga jarak dari orang yang batuk, dan tidak menyentuh
+      wajah dengan tangan yang tidak bersih adalah langkah yang disarankan untuk
+      mencegah penyakit ini.[23] Disarankan untuk menutup hidung dan mulut
+      dengan tisu atau siku yang tertekuk ketika batuk.[23] Organisasi Kesehatan
+      Dunia (WHO) dan Pusat Pengendalian dan Pencegahan Penyakit AS (CDC)
+      merekomendasikan kepada orang-orang yang menduga bahwa mereka telah
+      terinfeksi untuk memakai masker bedah dan mencari nasihat medis dengan
+      memanggil dokter dan tidak langsung mengunjungi klinik. Masker juga
+      direkomendasikan bagi mereka yang merawat seseorang yang diduga terinfeksi
+      tetapi tidak untuk digunakan masyarakat umum.[24][23] Belum ada vaksin
+      atau obat antivirus khusus untuk COVID-19; tata laksana yang diberikan
+      meliputi pengobatan terhadap gejala, perawatan suportif, dan tindakan
+      eksperimental.[25] Angka fatalitas kasus diperkirakan antara 1–3%.[26][27]
+    </p>
+  </section>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
