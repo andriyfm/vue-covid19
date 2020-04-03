3313961b998be2349f7fba53c8e9e765a2469934
Andri Firmansyah <andri@fairtech.com.sg>
2020-04-03 10:51:16 +0700

    Install and setup vue-router

--- src/main.js
+++ src/main.js
@@ -3,9 +3,11 @@ import Vue from "vue";
 import "./plugins";
 
 import App from "./App.vue";
+import router from './router'
 
 Vue.config.productionTip = false;
 
 new Vue({
+  router,
   render: h => h(App)
 }).$mount("#app");
