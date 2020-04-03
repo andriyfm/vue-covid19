5016c15a2e25c4057df69d56b4441f7fd78efbaa
andriyfm <andrifmsyh@gmail.com>
2020-04-03 10:59:55 +0700

    Update about page

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,12 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <AboutCovid />
   </div>
 </template>
+
+<script>
+import AboutCovid from "@/components/AboutCovid";
+export default {
+  components: { AboutCovid }
+};
+</script>
